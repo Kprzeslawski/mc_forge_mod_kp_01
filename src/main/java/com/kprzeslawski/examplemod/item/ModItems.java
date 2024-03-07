55b20dd598ed0e6294d245622e6dcba94a8546d4
Krzysztof <kprzeslawski01@gmail.com>
2024-03-07 19:50:20 +0100

    fix data and names

--- src/main/java/com/kprzeslawski/examplemod/item/ModItems.java
+++ src/main/java/com/kprzeslawski/examplemod/item/ModItems.java
@@ -14,7 +14,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MOD_ID);
-    public static final RegistryObject<Item> SW_1 = ITEMS.register("magic_sword",
+    public static final RegistryObject<Item> SW_1 = ITEMS.register("sw_1",
             () -> new ModedSwordItem(Tiers.NETHERITE,3, (float) 1f,new Item.Properties()));
 
     public static final RegistryObject<Item> TANGERINE = ITEMS.register("tangerine",
