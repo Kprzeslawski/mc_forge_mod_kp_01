df52f1c90b6c01bfa4f81d77408d56feeb0feb7b
Krzysztof <kprzeslawski01@gmail.com>
2024-03-11 20:06:03 +0100

    added display for upgrade requirement

--- src/main/java/com/kprzeslawski/examplemod/util/menu/EnergizedStationScreen.java
+++ src/main/java/com/kprzeslawski/examplemod/util/menu/EnergizedStationScreen.java
@@ -1,6 +1,7 @@
 package com.kprzeslawski.examplemod.util.menu;
 
 import com.kprzeslawski.examplemod.ExampleMod;
+import com.kprzeslawski.examplemod.item.modedItemClass.ModedSwordItem;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
@@ -8,6 +9,7 @@ import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public class EnergizedStationScreen extends AbstractContainerScreen<EnergizingStationMenu> {
@@ -28,6 +30,19 @@ public class EnergizedStationScreen extends AbstractContainerScreen<EnergizingSt
         int y = (height - imageHeight) / 2;
 
         guiGraphics.blit(TEXTURE, x, y, 0, 0, imageWidth, imageHeight);
+
+        ItemStack $$1 = ((EnergizingStationMenu)this.menu).getSlot(0).getItem();
+
+        if(!$$1.isEmpty()){
+            ModedSwordItem.EnergizeUpgradeCost upg =
+                    ((ModedSwordItem)$$1.getItem()).getNextUpgradeCost($$1);
+
+            if(upg.upg_count == 0 || upg.upgrade_crystal == null)return;
+            guiGraphics.drawString(this.font, "Required amount", x + 10, y + 20, 0);
+            guiGraphics.renderItem(new ItemStack(upg.upgrade_crystal, upg.upg_count), x+20, y+60);
+            guiGraphics.drawString(this.font, String.valueOf(upg.upg_count), x + 10, y + 40, 0);
+
+        }
     }
 
     @Override
