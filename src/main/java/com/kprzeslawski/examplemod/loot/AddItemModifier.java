98d4b86d10fb3ae69dec65f5c5b660cdb82c247b
Kprzeslawski <77459179+Kprzeslawski@users.noreply.github.com>
2024-06-19 22:08:23 +0200

    added conditions check

--- src/main/java/com/kprzeslawski/examplemod/loot/AddItemModifier.java
+++ src/main/java/com/kprzeslawski/examplemod/loot/AddItemModifier.java
@@ -30,6 +30,13 @@ public class AddItemModifier extends LootModifier {
 
     @Override
     protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
+
+        for(LootItemCondition condition : this.conditions){
+            if(!condition.test(context))
+                return generatedLoot;
+        }
+
+
         generatedLoot.add(new ItemStack(this.item));
         return generatedLoot;
     }
