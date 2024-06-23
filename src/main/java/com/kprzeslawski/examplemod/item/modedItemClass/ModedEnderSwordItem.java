11d7d28296338beb824053d5ed4efcda344da052
Kprzeslawski <77459179+Kprzeslawski@users.noreply.github.com>
2024-06-23 23:00:44 +0200

    make sword able to interact

--- src/main/java/com/kprzeslawski/examplemod/item/modedItemClass/ModedEnderSwordItem.java
+++ src/main/java/com/kprzeslawski/examplemod/item/modedItemClass/ModedEnderSwordItem.java
@@ -2,9 +2,16 @@ package com.kprzeslawski.examplemod.item.modedItemClass;
 
 import com.kprzeslawski.examplemod.item.modedItemClass.modedItemComponents.EnergizeUpgradeCost;
 import com.kprzeslawski.examplemod.item.modedItemClass.modedItemComponents.ReinforcedLevelProps;
+import com.kprzeslawski.examplemod.world.ModMobEffects;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -21,4 +28,15 @@ public class ModedEnderSwordItem  extends ModedSwordItem {
     public int getUseDuration(ItemStack pStack) {
         return 3000;
     }
+
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
+
+        ItemStack $$3 = pPlayer.getItemInHand(pHand);
+//        pPlayer.startUsingItem(pHand);
+//        pPlayer.addEffect(new MobEffectInstance(MobEffects.REGENERATION,9999));
+//        pPlayer.addEffect(new MobEffectInstance(ModMobEffects.BONUS_ARMOR.get(),9999));
+
+        return InteractionResultHolder.consume($$3);
+    }
+
 }
