001e8ed1b8c89a52d35d4cdd03314d0bc8a9f71e
Kprzeslawski <77459179+Kprzeslawski@users.noreply.github.com>
2024-06-26 22:31:09 +0200

    added new dimension

--- src/main/java/com/kprzeslawski/examplemod/datagen/ModWorldGenProvider.java
+++ src/main/java/com/kprzeslawski/examplemod/datagen/ModWorldGenProvider.java
@@ -4,6 +4,7 @@ import com.kprzeslawski.examplemod.ExampleMod;
 import com.kprzeslawski.examplemod.worldgen.ModBiomeModifiers;
 import com.kprzeslawski.examplemod.worldgen.ModConfiguredFeatures;
 import com.kprzeslawski.examplemod.worldgen.ModPlacedFeatures;
+import com.kprzeslawski.examplemod.worldgen.dimension.ModDimensions;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
@@ -16,9 +17,11 @@ import java.util.concurrent.CompletableFuture;
 
 public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.DIMENSION_TYPE, ModDimensions::bootstrapType)
             .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
             .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
-            .add(ForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
+            .add(ForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap)
+            .add(Registries.LEVEL_STEM, ModDimensions::bootstrapStem);
 
     public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(ExampleMod.MOD_ID));
