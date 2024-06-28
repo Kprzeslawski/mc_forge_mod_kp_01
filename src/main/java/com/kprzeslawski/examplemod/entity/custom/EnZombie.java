0263ff917a8816bfd6675e0fe6d8af9f8d185bc9
Kprzeslawski <77459179+Kprzeslawski@users.noreply.github.com>
2024-06-28 22:25:37 +0200

    change entity hp on hit

--- src/main/java/com/kprzeslawski/examplemod/entity/custom/EnZombie.java
+++ src/main/java/com/kprzeslawski/examplemod/entity/custom/EnZombie.java
@@ -1,6 +1,9 @@
 package com.kprzeslawski.examplemod.entity.custom;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -19,15 +22,37 @@ public class EnZombie extends Zombie {
     public static AttributeSupplier.Builder createAttributes() {
         return Monster.createMobAttributes()
                 .add(Attributes.FOLLOW_RANGE, 35.0)
-                .add(Attributes.MAX_HEALTH, 2D)
+                .add(Attributes.MAX_HEALTH, 20)
                 .add(Attributes.MOVEMENT_SPEED, 0.23000000417232513)
                 .add(Attributes.ATTACK_DAMAGE, 3.0)
                 .add(Attributes.ARMOR,2.0)
                 .add(Attributes.SPAWN_REINFORCEMENTS_CHANCE);
     }
 
+    @Override
+    public boolean hurt(DamageSource pSource, float pAmount) {
+        boolean doHurt = super.hurt(pSource, pAmount);
+        this.setCustomName(this.getNameAndHealthComponent());
+        return doHurt;
+    }
+
     public Component getNameAndHealthComponent(){
-        return Component.literal(displayedName + " " + this.healthString());
+        return Component
+                .literal(displayedName + " ")
+                .withStyle(ChatFormatting.AQUA)
+                .append(getHealthComponent());
+    }
+
+    public Component getHealthComponent(){
+        float currentHealth = this.getHealth();
+        float maxHealth = this.getMaxHealth();
+        ChatFormatting healthColor;
+        float percentageHealth = currentHealth / maxHealth;
+        if(percentageHealth > 0.7) healthColor = ChatFormatting.GREEN;
+        else if (percentageHealth > 0.3) healthColor = ChatFormatting.GOLD;
+        else healthColor = ChatFormatting.RED;
+
+        return Component.literal(healthString()).withStyle(healthColor);
     }
     public String healthString(){
         return this.getHealth() + "/" + this.getMaxHealth();
