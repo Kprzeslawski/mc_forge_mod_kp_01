da3e4ceee1dcb2cae8705570cfb411f4ed6824d2
Kprzeslawski <77459179+Kprzeslawski@users.noreply.github.com>
2024-06-29 13:49:25 +0200

    setup new attributes for different zombie levels

--- src/main/java/com/kprzeslawski/examplemod/entity/attributes/EnZombieAttributesBuilder.java
+++ src/main/java/com/kprzeslawski/examplemod/entity/attributes/EnZombieAttributesBuilder.java
@@ -18,4 +18,40 @@ public class EnZombieAttributesBuilder {
                 .add(Attributes.ATTACK_DAMAGE, 3.0)
                 .add(Attributes.ARMOR,2.0);
     }
+    public static AttributeSupplier.Builder attributesL2() {
+        return baseEnZombieAttributes()
+                .add(Attributes.MAX_HEALTH, 30)
+                .add(Attributes.MOVEMENT_SPEED, 0.23000000417232513)
+                .add(Attributes.ATTACK_DAMAGE, 4.0)
+                .add(Attributes.ARMOR,2.0);
+    }
+    public static AttributeSupplier.Builder attributesL3() {
+        return baseEnZombieAttributes()
+                .add(Attributes.MAX_HEALTH, 40)
+                .add(Attributes.MOVEMENT_SPEED, 0.23000000417232513)
+                .add(Attributes.ATTACK_DAMAGE, 5.0)
+                .add(Attributes.ARMOR,3.0);
+    }
+    public static AttributeSupplier.Builder attributesL4() {
+        return baseEnZombieAttributes()
+                .add(Attributes.MAX_HEALTH, 60)
+                .add(Attributes.MOVEMENT_SPEED, 0.33000000417232513)
+                .add(Attributes.ATTACK_DAMAGE, 6.0)
+                .add(Attributes.ARMOR,3.0);
+    }
+    public static AttributeSupplier.Builder attributesL5() {
+        return baseEnZombieAttributes()
+                .add(Attributes.MAX_HEALTH, 90)
+                .add(Attributes.MOVEMENT_SPEED, 0.33000000417232513)
+                .add(Attributes.ATTACK_DAMAGE, 8.0)
+                .add(Attributes.ARMOR,5.0);
+    }
+    public static AttributeSupplier.Builder attributesL6() {
+        return baseEnZombieAttributes()
+                .add(Attributes.MAX_HEALTH, 120)
+                .add(Attributes.MOVEMENT_SPEED, 0.43000000417232513)
+                .add(Attributes.ATTACK_DAMAGE, 10.0)
+                .add(Attributes.ARMOR,7.0);
+    }
+
 }
--- src/main/java/com/kprzeslawski/examplemod/event/ModEventBusEvents.java
+++ src/main/java/com/kprzeslawski/examplemod/event/ModEventBusEvents.java
@@ -14,10 +14,10 @@ public class ModEventBusEvents {
     @SubscribeEvent
     public static void registerAttributes(EntityAttributeCreationEvent event) {
         event.put(ModEntities.EN_ZOMBIE_L1.get(), EnZombieAttributesBuilder.attributesL1().build());
-        event.put(ModEntities.EN_ZOMBIE_L2.get(), EnZombieAttributesBuilder.attributesL1().build());
-        event.put(ModEntities.EN_ZOMBIE_L3.get(), EnZombieAttributesBuilder.attributesL1().build());
-        event.put(ModEntities.EN_ZOMBIE_L4.get(), EnZombieAttributesBuilder.attributesL1().build());
-        event.put(ModEntities.EN_ZOMBIE_L5.get(), EnZombieAttributesBuilder.attributesL1().build());
-        event.put(ModEntities.EN_ZOMBIE_L6.get(), EnZombieAttributesBuilder.attributesL1().build());
+        event.put(ModEntities.EN_ZOMBIE_L2.get(), EnZombieAttributesBuilder.attributesL2().build());
+        event.put(ModEntities.EN_ZOMBIE_L3.get(), EnZombieAttributesBuilder.attributesL3().build());
+        event.put(ModEntities.EN_ZOMBIE_L4.get(), EnZombieAttributesBuilder.attributesL4().build());
+        event.put(ModEntities.EN_ZOMBIE_L5.get(), EnZombieAttributesBuilder.attributesL5().build());
+        event.put(ModEntities.EN_ZOMBIE_L6.get(), EnZombieAttributesBuilder.attributesL6().build());
     }
 }
