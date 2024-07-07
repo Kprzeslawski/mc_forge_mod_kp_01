64fe6a713e1c03c05b1c83d58f5e12205069b2ca
Kprzeslawski <77459179+Kprzeslawski@users.noreply.github.com>
2024-07-07 22:33:09 +0200

    change example stats

--- src/main/java/com/kprzeslawski/examplemod/entity/attributes/EnSkeletonAttributeBuilder.java
+++ src/main/java/com/kprzeslawski/examplemod/entity/attributes/EnSkeletonAttributeBuilder.java
@@ -14,38 +14,38 @@ public class EnSkeletonAttributeBuilder {
     }
     public static AttributeSupplier.Builder attributesL2() {
         return Monster.createMobAttributes()
-                .add(Attributes.MAX_HEALTH, 20)
+                .add(Attributes.MAX_HEALTH, 40)
                 .add(Attributes.MOVEMENT_SPEED, 0.23000000417232513)
                 .add(Attributes.ATTACK_DAMAGE, 3.0)
-                .add(Attributes.ARMOR,2.0);
+                .add(Attributes.ARMOR,3.0);
     }
     public static AttributeSupplier.Builder attributesL3() {
         return Monster.createMobAttributes()
-                .add(Attributes.MAX_HEALTH, 20)
+                .add(Attributes.MAX_HEALTH, 60)
                 .add(Attributes.MOVEMENT_SPEED, 0.23000000417232513)
-                .add(Attributes.ATTACK_DAMAGE, 3.0)
-                .add(Attributes.ARMOR,2.0);
+                .add(Attributes.ATTACK_DAMAGE, 4.0)
+                .add(Attributes.ARMOR,3.0);
     }
     public static AttributeSupplier.Builder attributesL4() {
         return Monster.createMobAttributes()
-                .add(Attributes.MAX_HEALTH, 20)
+                .add(Attributes.MAX_HEALTH, 80)
                 .add(Attributes.MOVEMENT_SPEED, 0.23000000417232513)
-                .add(Attributes.ATTACK_DAMAGE, 3.0)
-                .add(Attributes.ARMOR,2.0);
+                .add(Attributes.ATTACK_DAMAGE, 4.0)
+                .add(Attributes.ARMOR,4.0);
     }
     public static AttributeSupplier.Builder attributesL5() {
         return Monster.createMobAttributes()
-                .add(Attributes.MAX_HEALTH, 20)
+                .add(Attributes.MAX_HEALTH, 100)
                 .add(Attributes.MOVEMENT_SPEED, 0.23000000417232513)
-                .add(Attributes.ATTACK_DAMAGE, 3.0)
-                .add(Attributes.ARMOR,2.0);
+                .add(Attributes.ATTACK_DAMAGE, 5.0)
+                .add(Attributes.ARMOR,4.0);
     }
     public static AttributeSupplier.Builder attributesL6() {
         return Monster.createMobAttributes()
-                .add(Attributes.MAX_HEALTH, 20)
+                .add(Attributes.MAX_HEALTH, 120)
                 .add(Attributes.MOVEMENT_SPEED, 0.23000000417232513)
-                .add(Attributes.ATTACK_DAMAGE, 3.0)
-                .add(Attributes.ARMOR,2.0);
+                .add(Attributes.ATTACK_DAMAGE, 7.0)
+                .add(Attributes.ARMOR,5.0);
     }
 
 }
